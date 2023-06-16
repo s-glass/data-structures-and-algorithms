7b0ba6d352b592c30c4aca374f2b0bae51ea9299
SARAH GLASS <SGLASS87@GMAIL.COM>
2023-06-16 16:25:42 -0700

    adding in node and linked list classes

--- python/tests/data_structures/test_linked_list.py
+++ python/tests/data_structures/test_linked_list.py
@@ -6,32 +6,32 @@ def test_exists():
     assert LinkedList
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_instantiate():
     assert LinkedList()
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_empty_head():
     linked = LinkedList()
     assert linked.head is None
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_populated_head():
     linked = LinkedList()
     linked.insert("apple")
     assert linked.head.value == "apple"
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_to_string_empty():
     linked_list = LinkedList()
 
     assert str(linked_list) == "NULL"
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_to_string_single():
     linked_list = LinkedList()
 
@@ -40,7 +40,7 @@ def test_to_string_single():
     assert str(linked_list) == "{ apple } -> NULL"
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_to_string_double():
     linked_list = LinkedList()
 
@@ -53,7 +53,7 @@ def test_to_string_double():
     assert str(linked_list) == "{ banana } -> { apple } -> NULL"
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_includes_true():
     linked_list = LinkedList()
 
@@ -64,7 +64,7 @@ def test_includes_true():
     assert linked_list.includes("apple")
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_includes_false():
     linked_list = LinkedList()
 
@@ -73,3 +73,36 @@ def test_includes_false():
     linked_list.insert("banana")
 
     assert not linked_list.includes("cucumber")
+
+"""
+The 4 tests at the bottom came from a comparison/review of tests prompted from AI via Bard
+"""
+
+def test_insert():
+    linked_list = LinkedList()
+    linked_list.insert("apple")
+    linked_list.insert("banana")
+    linked_list.insert("cucumber")
+    assert linked_list.head.value == "cucumber"
+    assert linked_list.head.next.value == "banana"
+    assert linked_list.head.next.next.value == "apple"
+    assert linked_list.head.next.next.next == None
+
+def test_empty_linked_list():
+  linked_list = LinkedList()
+  assert linked_list.head is None
+
+
+def test_insert_into_linked_list():
+  linked_list = LinkedList()
+  linked_list.insert(1)
+  assert linked_list.head.value == 1
+
+
+def test_includes_linked_list():
+  linked_list = LinkedList()
+  linked_list.insert(1)
+  linked_list.insert(2)
+  linked_list.insert(3)
+  assert linked_list.includes(2) is True
+  assert linked_list.includes(4) is False
