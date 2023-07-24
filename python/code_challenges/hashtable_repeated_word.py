a3404088a9b2352224df4d33c561061b5f47269d
SARAH GLASS <SGLASS87@GMAIL.COM>
2023-07-24 15:10:06 -0700

    cc31

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,5 +1,22 @@
 from data_structures.hashtable import Hashtable
 
+def first_repeated_word(input_string):
+    words = []
+    word_count = {}
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    first_repeat = None
+    for word in words:
+        word = word.lower()
+        if word_count[word] > 1:
+            first_repeat = word
+            break
+    return first_repeat
 
-def first_repeated_word():
-    pass
+
+__name__ == "__main__"
+input_str = "This is a test string with repeated words like is and a."
+first_repeated_word()
